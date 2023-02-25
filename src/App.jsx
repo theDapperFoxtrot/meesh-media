2b5ed652b99b8c6e69cad666cb28eb7df4af9710
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2023-02-25 12:00:38 +0200

    added sass / styling / content

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
 import { useState } from "react";
-import reactLogo from "./assets/svg/react.svg";
 import meeshLogo from "./assets/svg/meesh-media-logo.svg";
-import "./App.css";
+import purpleKeyboard from "./assets/images/purple-keyboard.jpg";
+import purpleSky from "./assets/images/purple-sky.jpg";
+import reactLogo from "./assets/svg/react.svg";
+import "./style/style.css";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<div className="App">
-			<div>
-				<a href="https://meeshmedia.com" target="_self">
-					<img src={meeshLogo} className="meesh-logo" alt="Meesh Media logo" />
-				</a>
+			<div className="body-container">
+				<div className="nav-container">
+					<a href="https://meeshmedia.com" target="_self">
+						<img src={meeshLogo} className="meesh-logo" alt="Meesh Media logo" />
+					</a>
+					<nav className="main-nav">
+						<a href="#">Home</a>
+						<a href="#">About</a>
+						<a href="#">Contact</a>
+					</nav>
+				</div>
+				<h1>Find everything you need to make your online presence stand out!</h1>
+				<div className="purple-keyboard-container">
+					<img className="purple-keyboard" src={purpleKeyboard} alt="purple keyboard" lazy />
+				</div>
+				<div className="card">
+					<h2>Digital Strategy Consultation</h2>
+					<h3>Advice on all the ways that you can grow your audience, harness the value of the customer advocate, and empower your business to achieve a well-known reputation.</h3>
+					<h2>Web Development</h2>
+					<h3>Everything from the inception of the design, to setting up the online hosting, arranging the domain name, updating the information, and tracking the interactions from visitors. It will all be taken care of, so you can focus on what you do best.</h3>
+					<h2>Social Media Management</h2>
+					<h3>There's too many social platforms out there. Leave it to an expert to manage it all on your behalf, and ensure your voice is being heard in all the crucial corners of the web.</h3>
+					<h2>This is a counter button!</h2>
+					<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				</div>
+				<footer>
+					<div className="purple-sky-container">
+						<img className="purple-sky" src={purpleSky} alt="purple sky" lazy />
+					</div>
+					<p>
+						Made with{" "}
+						<a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+							React
+							<img className="logo" src={reactLogo} alt="React logo" />
+						</a>{" "}
+						using{" "}
+						<a href="https://vitejs.dev/" target="_blank" rel="noopener noreferrer">
+							Vite
+							<img className="vite-logo" src="./vite.svg" alt="Vite logo" />
+						</a>
+					</p>
+				</footer>
 			</div>
-			<h6>Find everything you need to make your online presence stand out!</h6>
-			<div className="card">
-				<h2>This is a counter button!</h2>
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-			</div>
-			<p>
-				Made with{" "}
-				<a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-					React
-					<img className="logo" src={reactLogo} alt="React logo" />
-				</a>{" "}
-				using{" "}
-				<a href="https://vitejs.dev/" target="_blank" rel="noopener noreferrer">
-					Vite
-					<img className="vite-logo" src="./vite.svg" alt="Vite logo" />
-				</a>
-			</p>
 		</div>
 	);
 }
